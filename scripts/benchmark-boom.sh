ca2a87e09c3f6581d3332f77c6f316f4342824ad
Pete Bachant <petebachant@gmail.com>
2025-06-26 07:15:37 -0700

    Add TODO comments to script

--- scripts/benchmark-boom.sh
+++ scripts/benchmark-boom.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Remove any existing containers
+docker compose -f config/boom/compose.yaml down
+
 # Spin up BOOM services with Docker Compose
 docker compose -f config/boom/compose.yaml up -d
 
@@ -8,6 +11,10 @@ calkit xenv -n boom -- /app/kafka_producer ztf 20250614 public
 
 # Now time how long it takes for all of the alerts to be sent to the output
 # TODO: Send the logs somewhere so we can analyze later?
+
+# TODO: Wait until the workers are all done
+# Maybe we should watch an output stream and wait for all alerts to be
+# received?
 
 # Shut down the BOOM services
 docker compose -f config/boom/compose.yaml down
