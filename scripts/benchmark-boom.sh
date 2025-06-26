09f60ee9b9423c5dd537f066cd9672b623506334
Pete Bachant <petebachant@gmail.com>
2025-06-26 09:11:29 -0700

    Progress towards boom benchmarking working

--- scripts/benchmark-boom.sh
+++ scripts/benchmark-boom.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+COMPOSE_CONFIG="config/boom/compose.yaml"
+
 # Remove any existing containers
-docker compose -f config/boom/compose.yaml down
+docker compose -f $COMPOSE_CONFIG down
 
 # Spin up BOOM services with Docker Compose
-docker compose -f config/boom/compose.yaml up -d
+docker compose -f $COMPOSE_CONFIG up -d
 
-# Run Kakfa producer in the boom environment
-calkit xenv -n boom -- /app/kafka_producer ztf 20250614 public
-
-# Now time how long it takes for all of the alerts to be sent to the output
-# TODO: Send the logs somewhere so we can analyze later?
+# Send the logs to file so we can analyze later
+mkdir -p logs/boom
+docker compose -f $COMPOSE_CONFIG logs producer > logs/boom/producer.log &
+docker compose -f $COMPOSE_CONFIG logs consumer -f > logs/boom/consumer.log &
+docker compose -f $COMPOSE_CONFIG logs scheduler -f | tee logs/boom/scheduler.log
 
 # TODO: Wait until the workers are all done
 # Maybe we should watch an output stream and wait for all alerts to be
 # received?
+# Read the scheduler logs and wait for the filter worker to have zero tasks
+# left
 
 # Shut down the BOOM services
-docker compose -f config/boom/compose.yaml down
+docker compose -f $COMPOSE_CONFIG down
