b16bba55c39067375ecbfa9396b368c6b82f2733
Pete Bachant <petebachant@gmail.com>
2025-07-03 13:17:54 -0700

    Finish boom benchmarking as soon as we see empty queue 10 times in the logs

--- scripts/benchmark-boom.sh
+++ scripts/benchmark-boom.sh
@@ -12,13 +12,18 @@ docker compose -f $COMPOSE_CONFIG up -d
 mkdir -p logs/boom
 docker compose -f $COMPOSE_CONFIG logs producer > logs/boom/producer.log &
 docker compose -f $COMPOSE_CONFIG logs consumer -f > logs/boom/consumer.log &
-docker compose -f $COMPOSE_CONFIG logs scheduler -f | tee logs/boom/scheduler.log
+docker compose -f $COMPOSE_CONFIG logs scheduler -f > logs/boom/scheduler.log &
 
-# TODO: Wait until the workers are all done
-# Maybe we should watch an output stream and wait for all alerts to be
-# received?
-# Read the scheduler logs and wait for the filter worker to have zero tasks
-# left
+# Wait until we see "queue is empty" 10 times in the scheduler logs
+# This is not foolproof, but it should work for now
+# TODO: This should probably look at the filter worker
+# We could also potentially look at the size of the queues in Valkey
+echo "Waiting for all tasks to complete"
+while [ $(grep -c "queue is empty" logs/boom/scheduler.log) -lt 10 ]; do
+    sleep 1
+done
+
+echo "All tasks completed; shutting down BOOM services"
 
 # Shut down the BOOM services
 docker compose -f $COMPOSE_CONFIG down
