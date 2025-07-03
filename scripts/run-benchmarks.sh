881dcda7bf15730eb868f9cae1fcb7ea058c5fb8
Pete Bachant <petebachant@gmail.com>
2025-07-03 12:31:45 -0700

    Update pipeline and envs

--- scripts/run-benchmarks.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: This should actually run the benchmarks!
-echo "% This file was automatically generated; edits will be overwritten!" > paper/results.tex
-echo "\newcommand{\boomthroughputfactor}{TBD}" >> paper/results.tex
-echo "\newcommand{\kakfaingestratembps}{XXXXX}" >> paper/results.tex
-echo "\newcommand{\kakfaingestfactor}{XXXXX}" >> paper/results.tex
