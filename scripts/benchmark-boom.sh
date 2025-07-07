4d7024611e8ecb0d2dff36b040d745d5e6bc5819
Pete Bachant <petebachant@gmail.com>
2025-07-07 16:07:18 -0700

    Export stats from containers

--- scripts/benchmark-boom.sh
+++ scripts/benchmark-boom.sh
@@ -4,6 +4,7 @@ COMPOSE_CONFIG="config/boom/compose.yaml"
 
 # Remove any existing containers
 docker compose -f $COMPOSE_CONFIG down
+docker compose -f config/kowalski/compose.yaml down
 
 # Spin up BOOM services with Docker Compose
 docker compose -f $COMPOSE_CONFIG up -d
@@ -13,6 +14,9 @@ mkdir -p logs/boom
 docker compose -f $COMPOSE_CONFIG logs producer > logs/boom/producer.log &
 docker compose -f $COMPOSE_CONFIG logs consumer -f > logs/boom/consumer.log &
 docker compose -f $COMPOSE_CONFIG logs scheduler -f > logs/boom/scheduler.log &
+# Also log stats from containers for later analysis
+docker compose -f $COMPOSE_CONFIG stats consumer --format json > logs/boom/consumer.stats.log &
+docker compose -f $COMPOSE_CONFIG stats scheduler --format json > logs/boom/scheduler.stats.log &
 
 # Wait until we see "queue is empty" 10 times in the scheduler logs
 # This is not foolproof, but it should work for now
--- scripts/benchmark-kowalski.sh
+++ scripts/benchmark-kowalski.sh
@@ -4,22 +4,27 @@ COMPOSE_CONFIG="config/kowalski/compose.yaml"
 
 # Remove any existing containers
 docker compose -f $COMPOSE_CONFIG down
+docker compose -f config/boom/compose.yaml down
 
 # Spin up services with Docker Compose
 docker compose -f $COMPOSE_CONFIG up -d
 
 # Detect that all alerts have been processed
-# We know that for 20250311, there end up being ~639700 lines in the Dask
-# cluster log file
 # First wait for the file to be created
 echo "Waiting for Dask cluster log file to be created"
 while [ ! -f logs/kowalski/dask_cluster.log ]; do
     sleep 1
 done
-EXPECTED_LINES=639700
+
+# Send ingester container stats to log file
+docker compose -f $COMPOSE_CONFIG stats ingester --format json \
+    > logs/kowalski/ingester.stats.log &
+
+# Simply look for 'MLing' a certain number of times
+EXPECTED_ALERTS=29124
 echo "Waiting for all tasks to complete"
-while [ $(wc -l < logs/kowalski/dask_cluster.log) -lt $EXPECTED_LINES ]; do
-    sleep 10
+while [ $(grep -c MLing logs/kowalski/dask_cluster.log) -lt $EXPECTED_ALERTS ]; do
+    sleep 1
 done
 
 # Shut down the services
