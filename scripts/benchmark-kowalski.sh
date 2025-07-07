703b9bb33a634f25357f7c64e1d042d72e51d6c5
Pete Bachant <petebachant@gmail.com>
2025-07-07 14:59:22 -0700

    Run pipeline

--- scripts/benchmark-kowalski.sh
+++ scripts/benchmark-kowalski.sh
@@ -8,11 +8,19 @@ docker compose -f $COMPOSE_CONFIG down
 # Spin up services with Docker Compose
 docker compose -f $COMPOSE_CONFIG up -d
 
-# TODO: Wait until the workers are all done
-# Maybe we should watch an output stream and wait for all alerts to be
-# received?
-# Read the scheduler logs and wait for the filter worker to have zero tasks
-# left
+# Detect that all alerts have been processed
+# We know that for 20250311, there end up being ~639700 lines in the Dask
+# cluster log file
+# First wait for the file to be created
+echo "Waiting for Dask cluster log file to be created"
+while [ ! -f logs/kowalski/dask_cluster.log ]; do
+    sleep 1
+done
+EXPECTED_LINES=639700
+echo "Waiting for all tasks to complete"
+while [ $(wc -l < logs/kowalski/dask_cluster.log) -lt $EXPECTED_LINES ]; do
+    sleep 10
+done
 
 # Shut down the services
 docker compose -f $COMPOSE_CONFIG down
