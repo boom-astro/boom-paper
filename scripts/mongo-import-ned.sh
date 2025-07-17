ad55d467c7fa8bb46cb6c1801afb7c3061910d89
Theophile du Laz <theophile.dulaz@gmail.com>
2025-07-17 14:40:44 -0700

    A salad of bugfixes and improvements (#9)

--- scripts/mongo-import-ned.sh
+++ scripts/mongo-import-ned.sh
@@ -5,6 +5,11 @@ echo "Importing NED alerts into $DB_NAME MongoDB database"
 gunzip -kc /kowalski.NED.json.gz | \
     mongoimport \
     "mongodb://mongoadmin:mongoadminsecret@mongo:27017/$DB_NAME?authSource=admin$DB_ADD_URI" \
-    --collection NED_alerts \
+    --collection NED \
     --jsonArray \
     --drop
+
+# then, create a 2d index on coordinates.radec_geojson
+echo "Creating 2d index on coordinates.radec_geojson"
+mongosh "mongodb://mongoadmin:mongoadminsecret@mongo:27017/$DB_NAME?authSource=admin" \
+    --eval "db.NED.createIndex({ 'coordinates.radec_geojson': '2dsphere' })"
